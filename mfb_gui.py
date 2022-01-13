780b4edb52b3d7d7412be8f338bfebe65ca165ba
r-tnk <r_tanaka@sci.hokudai.ac.jp>
2022-01-13 17:21:13 +0900

    read settings.ini and show it

--- mfb_gui.py
+++ mfb_gui.py
@@ -3,8 +3,8 @@ import mfb
 
 global settings, dicti_vars
 layout = [
-   [sg.T("Choose setting file(*.ini)"), sg.InputText("Choose setting file(*.ini)", key='-file-', enable_events=True), sg.FileBrowse(key="ini_path")],
-   [sg.Combo(values=[''], key='SECTION')],
+   [sg.InputText("Choose setting file(*.ini)", key='-file-', enable_events=True), sg.FileBrowse(key="ini_path")],
+   [sg.Combo(values=[''], size=(20,1), key='SECTION', enable_events=True)],
    [sg.Button('GO'), sg.Button('Cancel')],
 ]
 
@@ -13,7 +13,6 @@ dict_vars = {}
 mfb.set_smooth(dict_vars)
 section_name = 'SMOOTH5'
 
-# イベントループを使用してウィンドウを表示し、対話する
 while True:
     event, values = window.read()
     if event == sg.WINDOW_CLOSED or event == 'Cancel':
@@ -23,6 +22,9 @@ while True:
         config = mfb.read_ini(values['ini_path'])
         print(config.sections())
         window.find_element('SECTION').Update(values=config.sections())
+    
+    if event == 'SECTION':
+        for key in config[values['SECTION']]:
+            print(key, config[values['SECTION']][key])
 
-# 画面から削除して終了
 window.close()
